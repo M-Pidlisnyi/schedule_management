e26dc97f15234057d51df3c9345dbc0c4a4896ae
M-Pidlisnyi <mishapropatriot@gmail.com>
2025-06-19 20:29:19 +0300

    created models.

---
+++ main.py
@@ -0,0 +1,17 @@
+import django_setup
+from schedule.models import Schedule, Student, Subject, Teacher, Grade, Group
+
+print(""" 
+1. Create Teacher
+2. Create Student
+3. Get all subjects      
+...
+""")
+
+user_inpur = input()
+
+if user_inpur == "1":
+    ...
+elif user_inpur == "2":
+    ...
+
--- schedule/models.py
+++ schedule/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
+class Subject(models.Model):
+    title = models.CharField(max_length=50)
+
+
+class Teacher(models.Model):
+    name = models.CharField(max_length=50)
+
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE, related_name="teachers")
+    
+
+class Group(models.Model):
+    year = models.PositiveIntegerField(validators=[MaxValueValidator(12), MinValueValidator(1)])#7
+    code = models.CharField(max_length=1)#Б
+    
+
+    def __str__(self):
+        return f"{self.year}-{self.code}"#7-Б
+    
+class Student(models.Model):
+    name = models.CharField(max_length=50)
+
+    group  = models.ForeignKey(Group, on_delete=models.SET_NULL, null=True)
+
+
+class Schedule(models.Model):
+    teacher = models.ForeignKey(Teacher, on_delete=models.SET_NULL, null=True)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+
+    time = models.TimeField()
+
+
+class Grade(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+
+    value = models.PositiveIntegerField(validators=[MaxValueValidator(12), MinValueValidator(1)])
+    
